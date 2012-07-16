0590fbbd4d8285e1da5c6884814ac6d66f9a7887
Koala Yeung <koalay@gmail.com>
2012-07-16 15:53:21 +0800

    first commit

---
+++ realpath/realpath.go
@@ -0,0 +1,87 @@
+package realpath
+
+import (
+  "os"
+  "fmt"
+  "bytes"
+)
+
+func Realpath(filepath string) (string, error) {
+
+  if len(filepath)==0 { return "", os.ErrInvalid }
+
+  sepStr := string(os.PathSeparator)
+
+  if filepath[0] != os.PathSeparator {
+    pwd, err := os.Getwd()
+    if err!=nil { return "", err }
+    filepath = pwd + sepStr + filepath
+  }
+
+  path   := []byte(filepath)
+  nlinks := 0
+  start  := 1
+  prev   := 1
+  for start < len(path) {
+    c   := nextComponent(path, start)
+    cur := c[start:]
+
+    switch {
+
+      case len(cur)==0:
+        copy(path[start:], path[start+1:])
+        path = path[0:len(path)-1]
+
+      case len(cur)==1 && cur[0]=='.':
+        if start+2 < len(path) { copy(path[start:], path[start+2:]) }
+        path = path[0:len(path)-2]
+
+      case len(cur)==2 && cur[0]=='.' && cur[1] == '.':
+        copy(path[prev:], path[start+2:])
+        path = path[0:len(path)+prev-(start+2)]
+        prev  = 1
+        start = 1
+
+      default:
+
+        fi, err := os.Lstat(string(c))
+        if err!=nil { return "", err }
+        if fi.Mode() & os.ModeSymlink == os.ModeSymlink {
+
+          nlinks++
+          if nlinks > 16 { return "", os.ErrInvalid }
+
+          var dst string
+          dst, err = os.Readlink(string(c))
+          fmt.Printf("SYMLINK -> %s\n", dst)
+
+          rest := string(path[len(c):])
+          if dst[0] == '/' {
+            // Absolute links
+            path  = []byte(dst + "/" + rest)
+          } else {
+            // Relative links
+            path  = []byte(string(path[0:start])+dst+"/"+rest)
+          }
+          prev  = 1
+          start = 1
+        } else {
+          // Directories
+          prev  = start
+          start = len(c) + 1
+        }
+    }
+  }
+
+  for len(path)>1 && path[len(path)-1]=='/' { path = path[0:len(path)-1] }
+  return string(path), nil
+
+}
+
+func nextComponent(path []byte, start int) []byte {
+  v := bytes.IndexByte(path[start:], '/')
+  if v < 0 { return path }
+  return path[0 : start+v]
+}
+
+
